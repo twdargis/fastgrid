a3af9837c32a51e2252092c7732014247c71246f
Opsis <ted@opsisllc.com>
2019-04-06 10:18:32 -0500

    Inital commit.

--- header.php
+++ header.php
@@ -1,1 +1,19 @@
-header.php
+<html>
+
+<head>
+
+<title>Tutorial theme</title>
+
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+</head>
+
+<body>
+
+<div id="wrapper">
+
+<div id="header">
+
+<h1>HEADER</h1>
+
+</div>
